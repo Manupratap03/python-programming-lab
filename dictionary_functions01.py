754611502fceb38063dbc4e82d41645c9a0c93a6
Manupratap03 <105970522+Manupratap03@users.noreply.github.com>
2022-05-28 20:27:41 +0530

    Create dictionary_functions01.py

---
+++ dictionary_functions01.py
@@ -0,0 +1,9 @@
+myDict = {
+    "Fast": "In a Quick Manner",
+    "Manu": "A Student",
+    "Marks": [1, 2, 3],
+    "AnotherDict": {"Name": "Manu", "section": "J"}
+}
+print(type(myDict.keys()))          #We can convert the type by using typecasting
+print(list(myDict.keys()))          #Typecasted into list format
+print(type(list(myDict.keys())))
